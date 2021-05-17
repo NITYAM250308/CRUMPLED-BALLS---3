c7da54c8b8c31e037c4e22566f87f2f9e12be7d6
NITYAM250308 <79703336+NITYAM250308@users.noreply.github.com>
2021-05-17 20:06:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var binImage,binImage
+function preload() {
+	binImage=loadImage("images/dustbingreen.png");
+}
+function setup() {
+	createCanvas(690, 688);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	bin=createSprite(600,575,100,50);
+	bin.addImage(binImage);
+	bin.scale=0.45
+
+	//Create the Bodies Here.
+	ground=new Ground(400,680,800,20);
+	paper=new Paper(100,600,10);
+
+	rightSide=new Dustbin(664,585,20,170);
+	leftSide=new Dustbin(534,585,20,170);
+	bottom=new Dustbin(600,660,125,20);
+
+	launcher = new Launcher(paper.body,{x:100, y:500});
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  Engine.update(engine);
+  background(0,255,255);
+  
+
+  paper.display();
+
+  ground.display();
+
+  bottom.display();
+  rightSide.display();
+  leftSide.display();
+
+  bin.display();
+
+  launcher.display();
+  
+  drawSprites();
+ 
+}
+
+
+function mouseDragged(){
+    Matter.Body.setPosition(paper.body,{x: mouseX, y: mouseY});
+
+}
+
+function mouseReleased(){
+    launcher.fly()
+}
+
+function keyPressed(){
+	if(keyCode === 32){
+		launcher.attach(paper.body)
+	}
+}
+
+
